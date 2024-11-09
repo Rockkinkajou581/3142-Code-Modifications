3cc5dfb5467bbe965a11cc41e65e8d5e19601187
Rockkinkajou581 <151485808+Rockkinkajou581@users.noreply.github.com>
2024-11-08 22:25:59 -0500

    Create Gainscheduler.cpp

---
+++ Gainscheduler.cpp
@@ -0,0 +1,16 @@
+#include "lemlib\chassis\gainscheduler.hpp"
+float Gainvalues::findgain(float initialerror)  {
+    if(fabs(initialerror) <= xvalues[0]) {
+        return yvalues[0];
+    }
+    for (int i = 1; i <= xvalues.size() - 1; ++i) {
+        if(fabs(initialerror) <= xvalues[i]) {
+            return lerp(initialerror, i - 1, i);
+        }
+    }
+    return yvalues[yvalues.size() - 1];
+}
+float Gainvalues::lerp(float initialerror, int x1, int x2) {
+    float m = (yvalues[x2] - yvalues[x1])/(xvalues[x2] - xvalues[x1]);
+    return (fabs(initialerror) - xvalues[x1])*m + yvalues[x1];
+}
